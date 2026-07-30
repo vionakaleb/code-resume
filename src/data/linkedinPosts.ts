6b877f55d6cc03babeb5243b4db134807d73593b
Viona Kaleb <vionakaleb@gmail.com>
2026-07-30 14:30:26 +0700

    Fix embed post source

--- src/data/linkedinPosts.ts
+++ src/data/linkedinPosts.ts
@@ -17,7 +17,7 @@ export const linkedinPosts: LinkedInPost[] = [
   {
     id: "urn:li:share:7482293313488728064",
     link: "https://www.linkedin.com/posts/vionakaleb_sistech2026-womenintech-frontendengineering-activity-7482293314692730880-4r2-?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAABtfvw0BEeCfYmRN9CmBBlWYbyrfcEb7mzg",
-    embedUrl: `${LINKEDIN_EMBED_BASE}/urn:li:ugcPost:7482293313488728064`,
+    embedUrl: `${LINKEDIN_EMBED_BASE}/urn:li:share:7482293313488728064`,
     collapsedHeight: 560,
   },
   {
